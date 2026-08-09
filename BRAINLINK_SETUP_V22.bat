473973928f6011e2097dba6bf96ea12bd6c654cd
MRTNLGDR <lucas@martynlegrand.com>
2026-08-08 22:03:04 -0300

    feat: add Brainlink v2.2 Windows setup entrypoint

---
+++ BRAINLINK_SETUP_V22.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+where powershell >nul 2>nul || (echo [BRAINLINK] PowerShell is required.& exit /b 1)
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\materialize-brainlink-v22.ps1" -Install
+exit /b %errorlevel%
